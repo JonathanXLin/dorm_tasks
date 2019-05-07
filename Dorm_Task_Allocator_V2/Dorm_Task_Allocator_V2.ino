61feb0b89f1b9d4ef7a203accec8d1d398c58092
Jonathan Lin <jlin1999@gmail.com>
2019-05-06 21:00:32 -0400

    Code Edit for OLED

--- Dorm_Task_Allocator_V2/Dorm_Task_Allocator_V2.ino
+++ Dorm_Task_Allocator_V2/Dorm_Task_Allocator_V2.ino
@@ -1,33 +1,45 @@
-#include <LiquidCrystal.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define button A6
-#define buzzer 10
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+int button = 2;
+int buzzer = 3;
 
-String names[] = {"Jonathan", "Gordon", "Oscar", "Misha", "David"};
-int numNames = 5;
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+String names[] = {"Jonathan", "Gordon", "Oscar", "David"};
+int numNames = 4;
 
 char score[] = "g1s1d1d1e1s1d1s3F1s1g1s3";
 
 void setup() {
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.display();
+  
   pinMode(button, INPUT);
   pinMode(buzzer, OUTPUT);
-  
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
 
   Serial.begin(9600);
+  Serial.setTimeout(10);
 }
 
 int currName = 0;
 int pressed = 0;//Toggled to 1 if press is detected in cycle
 
 void loop() {
-  Serial.println(analogRead(button));
-  
-  if (analogRead(button) == 1023)
+  if (digitalRead(button))
   {
     //Cycles through names on button press  
     if (currName == numNames - 1)
@@ -37,14 +49,6 @@ void loop() {
 
     pressed = 1;
   }
-
-  // Print name to the LCD.
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Next task for:");
-  lcd.setCursor(0, 1);
-  lcd.print(names[currName]);
-  lcd.display();
 
   //600ms delay if pressed to avoid cycling by more than 1 per button press
   if (pressed)
@@ -60,6 +64,19 @@ void loop() {
       delay(30);
     }
     pressed = 0;
+
+    // Print name to the OLED.
+    display.clearDisplay();
+    
+    if (currName == 0)
+      display.setTextSize(2);
+    else
+      display.setTextSize(3);
+      
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println(names[currName]);
+    display.display();
   }
   else
     delay(100);
